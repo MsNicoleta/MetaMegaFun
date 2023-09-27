77fd91f0587356860958f8ad1c74149fa9f655a0
Nicoleta Grubi <msnicoleta@outlook.com>
2023-09-27 15:05:13 +0100

    navBar completed with icons and background color

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,8 +14,17 @@ const Navbar = () => (
     whileInView="show" // Animate when in view
     className={`${styles.xPaddings} py-8 relative`}
   >
-    Navbar
-
+    <div className="absolute w-[50%] inset-0 gradient-01" />
+    <div className={`${styles.innerWidth} mx-auto
+      flex justify-between gap-8`}>
+      <img
+        src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contain"
+      />
+      <h2 className="font-bold text-[24px] leading-[30px] text-white">
+        Meta Mega Fun
+      </h2>
+      <img src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain" />
+    </div>
   </motion.nav>
 );
 
