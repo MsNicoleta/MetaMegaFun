9b8b4dbce1ed09f6e28032456aefa376a79ee957
Nicoleta Grubi <msnicoleta@outlook.com>
2023-09-28 23:59:40 +0100

    add text animation to the about section

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -2,17 +2,20 @@
 
 import { motion } from 'framer-motion';
 
-import { textContainer } from '../utils/motion.js';
-
-
-import { textContainer } from "../utils/motion";
+import { textContainer, textVariant2 } from '../utils/motion';
 
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
-    variants={textContainer(1.1)}
-    className={textStyles}
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
   >
-    Typing Text
+    {Array.from(title).map((letter, index) => (
+      <motion.span variants={textVariant2} key={index}
+      >
+        {letter === ' ' ? '\u00A0' : letter}
+      </motion.span>
+    ))}
+
 
   </motion.p>
 );
