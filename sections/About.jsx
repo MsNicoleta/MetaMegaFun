f0d7af84e7f2ac65cb3d62223b60d88470ace5f9
Nicoleta Grubi <msnicoleta@outlook.com>
2023-09-28 23:31:47 +0100

    starting the About section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,5 +1,10 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { navVariants } from '../utils/motion.js';
+
 const About = () => (
   <section>
     About section
