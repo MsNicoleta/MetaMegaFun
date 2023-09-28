8c905fd0155663425e24944437d366d8a0e3bbd0
Nicoleta Grubi <msnicoleta@outlook.com>
2023-09-28 23:47:45 +0100

    add a background gradient

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,22 @@
 'use client';
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { motion } from 'framer-motion';
+
+import { textContainer } from '../utils/motion.js';
+
+
+import { textContainer } from "../utils/motion";
+
+export const TypingText = ({ title, textStyles }) => (
+  <motion.p
+    variants={textContainer(1.1)}
+    className={textStyles}
+  >
+    Typing Text
+
+  </motion.p>
 );
 
-export const TitleText = () => (
+export const TitleText = ({ title, textStyles }) => (
   <h2>Title Text</h2>
 );
--- sections/About.jsx
+++ sections/About.jsx
@@ -1,13 +1,24 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import { TypingText } from "../components";
 
 import styles from '../styles';
-import { navVariants } from '../utils/motion.js';
+import { fadeIn, staggerContainer } from '../utils/motion.js';
 
 const About = () => (
-  <section>
-    About section
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      animate="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+      About section
+    >
+      <TypingText title="| About MetaMegaFun" textStyles="text-center" />
+    </motion.div>
   </section>
 );
 
