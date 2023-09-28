2c29462bb9280d0b9893761b630dd777f1050e9a
Nicoleta Grubi <msnicoleta@outlook.com>
2023-09-28 15:29:02 +0100

    starting the Hero section

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,9 +1,21 @@
 'use client';
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion.js';
 
 const Hero = () => (
-  <section>
-    Hero section
-  </section>
+  <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <div className="flex justify-content items-center flex-col relative z-10 ">
+    </motion.div>
+  </section >
 );
 
 export default Hero;
