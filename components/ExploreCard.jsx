00cf5c089bf569970bee0b7f1bbfcf0238311da9
Nicoleta Grubi <msnicoleta@outlook.com>
2023-09-29 15:15:54 +0100

    ###  Mapping through an array of 'exploreWorlds'added

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,5 +1,9 @@
 'use client';
 
+import { motion } from 'framer-motion'; // Import animations from Framer Motion library
+import styles from '../styles'; // Import styles for consistent styling
+import { fadeIn } from '../utils/motion.js'; // Import animation variants from a utility file
+
 const ExploreCard = () => (
   <div>
     Explore Card
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -6,26 +6,42 @@ import { staggerContainer } from '../utils/motion.js';
 import { ExploreCard, TypingText, TitleText } from "../components";
 import { exploreWorlds } from "../constants";
 
-const Explore = () => (
-  <section className={`${styles.paddings}`} id="explore">
-    {/*the explore constant will allow us to scroll when pressing on our stamp image */}
-    <motion.div
-      // Apply the motion animation with the 'staggerContainer' variant
-      variants={staggerContainer}
-      // Set the initial animation state to 'hidden'
-      initial="hidden"
-      // Trigger animation when the component is in view
-      whileInView="show"
-      // Customize the viewport settings for animation
-      viewport={{ once: false, amount: 0.25 }}
-      // Add CSS classes and styles to the component
-      className={`${styles.innerWidth} mx-auto flex flex-col`}
-    >
-      <TypingText title="| The World" textStyles="text-center" />
-      {/* render the typing text effect component   */}
-      <TitleText title={<> Choose the world you want <br className="md:block hidden" /> to explore </>} textStyles="text-center" />
-      {/* render the explore card component for the   */}
-    </motion.div>
-  </section>
-);
+const Explore = () => {
+  const [active, setActive] = useState('world-2');
+  return (
+    <section className={`${styles.paddings}`}
+      id="explore">
+      {/*the explore constant will allow us to scroll when pressing on our stamp image */}
+      <motion.div
+        // Apply the motion animation with the 'staggerContainer' variant
+        variants={staggerContainer}
+        // Set the initial animation state to 'hidden'
+        initial="hidden"
+        // Trigger animation when the component is in view
+        whileInView="show"
+        // Customize the viewport settings for animation
+        viewport={{ once: false, amount: 0.25 }}
+        // Add CSS classes and styles to the component
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="| The World" textStyles="text-center" />
+        {/* render the typing text effect component   */}
+        <TitleText title={<> Choose the world you want <br className="md:block hidden" /> to explore </>} textStyles="text-center" />
+        {/* render the explore card component for the   */}
+        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5]">
+          {/* Mapping through an array of 'exploreWorlds' */}
+          {exploreWorlds.map((world, index) => (
+            <ExploreCard
+              key={world.id}             // Unique 'key' prop for React
+              {...world}                // Spread the 'world' object properties as props
+              index={index}             // Prop 'index' to track the current index in the loop
+              active={active}           // Prop 'active' to pass the 'active' state
+              handleClick={setActive}   // Prop 'handleClick' to pass a click handler function
+            />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  )
+};
 export default Explore;
