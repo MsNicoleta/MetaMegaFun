a71cebc9d8a1f58dc5130c08f6d5fc0ea24681d8
Nicoleta Grubi <msnicoleta@outlook.com>
2023-09-29 13:44:01 +0100

    ### Component for rendering a title text with motion animations (TitleText)

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,25 +1,32 @@
+
 'use client';
 
 import { motion } from 'framer-motion';
 
 import { textContainer, textVariant2 } from '../utils/motion';
 
+// Component for rendering a typewriter effect on text
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
     variants={textContainer}
     className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
   >
     {Array.from(title).map((letter, index) => (
-      <motion.span variants={textVariant2} key={index}
-      >
+      <motion.span variants={textVariant2} key={index}>
         {letter === ' ' ? '\u00A0' : letter}
       </motion.span>
     ))}
-
-
   </motion.p>
 );
 
+// Component for rendering a title text with motion animations
 export const TitleText = ({ title, textStyles }) => (
-  <h2>Title Text</h2>
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+  >
+    {title}
+  </motion.h2>
 );
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -8,23 +8,24 @@ import { exploreWorlds } from "../constants";
 
 const Explore = () => (
   <section className={`${styles.paddings}`} id="explore">
-    Explore section
+    {/*the explore constant will allow us to scroll when pressing on our stamp image */}
+    <motion.div
+      // Apply the motion animation with the 'staggerContainer' variant
+      variants={staggerContainer}
+      // Set the initial animation state to 'hidden'
+      initial="hidden"
+      // Trigger animation when the component is in view
+      whileInView="show"
+      // Customize the viewport settings for animation
+      viewport={{ once: false, amount: 0.25 }}
+      // Add CSS classes and styles to the component
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| The World" textStyles="text-center" />
+      {/* render the typing text effect component   */}
+      <TitleText title={<> Choose the world you want <br className="md:block hidden" /> to explore </>} textStyles="text-center" />
+      {/* render the explore card component for the   */}
+    </motion.div>
   </section>
 );
-// the explore constant will allow us to scroll when pressing on our stamp image
-
-<motion.div
-  // Apply the motion animation with the 'staggerContainer' variant
-  variants={staggerContainer}
-  // Set the initial animation state to 'hidden'
-  initial="hidden"
-  // Trigger animation when the component is in view
-  whileInView="show"
-  // Customize the viewport settings for animation
-  viewport={{ once: false, amount: 0.25 }}
-  // Add CSS classes and styles to the component
-  className={`${styles.innerWidth} mx-auto flex flex-col`}
->
-  {/* Content of the motion.div goes here */}
-</motion.div>
 export default Explore;
