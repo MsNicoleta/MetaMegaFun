1c70b17c91b8a423ccf27b389f67ae390416d796
Nicoleta Grubi <msnicoleta@outlook.com>
2023-09-29 12:56:05 +0100

    ### Added cooments on the Hero seaction

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,26 +1,31 @@
+// Import necessary modules and components
 'use client';
-import { motion } from 'framer-motion';
+import { motion } from 'framer-motion'; // Import animations from Framer Motion library
+import styles from '../styles'; // Import styles for consistent styling
+import { slideIn, staggerContainer, textVariant } from '../utils/motion.js'; // Import animation utilities
 
-import styles from '../styles';
-import { slideIn, staggerContainer, textVariant } from '../utils/motion.js';
-
+// Define the Hero component
 const Hero = () => (
   <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
     <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
+      variants={staggerContainer} // Use animation variants defined in motion.js utility
+      initial="hidden" // Set initial animation state
+      whileInView="show" // Trigger animation when component is in view
+      viewport={{ once: false, amount: 0.25 }} // Define animation viewport settings
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
+      {/* Hero content */}
       <div className="flex justify-center items-center flex-col relative z-10">
+        {/* Animated text with textVariant animation */}
         <motion.h1
-          variants={textVariant(1.1)}
+          variants={textVariant(1.1)} // Use textVariant animation with specified settings
           className={styles.heroHeading}>
           Meta
         </motion.h1>
+
+        {/* Animated text with textVariant animation */}
         <motion.div
-          variants={textVariant(1.2)}
+          variants={textVariant(1.2)} // Use textVariant animation with specified settings
           className="flex flex-row justify-center items-center "
         >
           <h1 className={styles.heroHeading}>Ma</h1>
@@ -28,14 +33,26 @@ const Hero = () => (
           <h1 className={styles.heroHeading}>Ness</h1>
         </motion.div>
       </div>
+
+      {/* Image and content */}
       <motion.div
-        variants={slideIn('right', 'tween', 0.2, 1)}
+        variants={slideIn('right', 'tween', 0.2, 1)} // Use slideIn animation with specified settings
         className="relative w-full md:mt-[20px]-mt[12px]"
       >
+        {/* Gradient background with rounded corners */}
         <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]" />
-        <img src="/cover.png" alt="cover-hero" className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-[10] relative" />
+
+        {/* Main image */}
+        <img
+          src="/cover.png"
+          alt="cover-hero"
+          className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-[10] relative"
+        />
+
+        {/* Link to explore section */}
         <a href="#explore">
           <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10 ">
+            {/* Stamp image */}
             <img
               src="/stamp.png"
               alt="stamp"
@@ -45,7 +62,7 @@ const Hero = () => (
         </a>
       </motion.div>
     </motion.div>
-  </section >
+  </section>
 );
 
 export default Hero;
