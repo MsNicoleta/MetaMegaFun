e03f3fff9370ba3006e4818e80d574b794853f6e
Nicoleta Grubi <msnicoleta@outlook.com>
2023-09-29 11:40:44 +0100

    about section - added text description of the metaverse styled the boldness- arrow-down animation added too

--- sections/About.jsx
+++ sections/About.jsx
@@ -18,6 +18,15 @@ const About = () => (
       About section
     >
       <TypingText title="| About MetaMegaFun" textStyles="text-center" />
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white">
+        <span className="font-bold text-white"> Metaverse Mega Fun </span>is a new thing in the future, where you can enjoy the virtual world by feeling like it's really real, you can feel what you feel in this metaverse world, because this is really the <span className="font-bold text-white"> madness of the metaverse</span> of today, using only <span className="font-bold text-white"> VR </span>devices you can easily explore the metaverse world you want, turn your dreams into reality. Let's <span className="font-bold text-white">explore</span> the madness of the metaverse by scrolling down
+      </motion.p>
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg" alt="arrow-down" className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
     </motion.div>
   </section>
 );
