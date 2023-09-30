0c3dc7b0fddad794265106cdfe40bd5d746513f8
Nicoleta Grubi <msnicoleta@outlook.com>
2023-09-30 23:57:12 +0100

    Starting with WhatsNew page

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { motion } from 'framer-motion'; // Import motion from Framer Motion
+import styles from '../styles'; // Import styles (ensure the correct path)
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion.js'; // Import motion variants
+import { NewFeatures, TypingText, TitleText } from "../components"; // Import components
+import { newFeatures } from "../constants"; // Import constants
+
+
 const WhatsNew = () => (
   <section>
     What's new section
