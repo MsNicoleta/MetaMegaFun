66965bea2f36c6ebe399f2c2e0621eaafe30b7d6
Nicoleta Grubi <msnicoleta@outlook.com>
2023-09-30 20:07:01 +0100

    ### Added the vr set image and the semitransparent-container around it

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -18,9 +18,12 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
         {title}
       </h3>
     ) : (
-      <div>
-        <div>
-          <img src='/headset.svg' />
+      <div className='absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]' >
+        <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}>
+          <img src='/headset.svg'
+            alt="headset"
+            className='w-1/2 h-1/2 object-contain'
+          />
         </div>
       </div>
     )}
