4ad3dd91a3c3470f98b3638e86f07d32f3e92e4a
Nicoleta Grubi <msnicoleta@outlook.com>
2023-10-04 14:26:02 +0100

    added the Title to the two columns and the relative icons also aranged them to be dinamic based on the size of the screen

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -2,7 +2,7 @@ import styles from '../styles';
 
 const StartSteps = ({ number, text }) => (
   <div className={`${styles.flexCenter} flex-row z-10`}>
-    <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#4e597481]`}>
+    <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}>
       <p className='font-bold text-[20px] text-white'>0{number}
       </p>
     </div>
