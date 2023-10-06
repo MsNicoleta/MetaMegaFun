75828b9f8f5436e7536d4230a8d5e2cb2c9bd5c9
Nicoleta Grubi <msnicoleta@outlook.com>
2023-10-06 16:52:25 +0100

    insight section - added the images for the different worlds

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,10 +1,22 @@
 'use client';
 import { motion } from "framer-motion";
 import { fadeIn } from '../utils/motion';
-const InsightCard = (imgUrl) => (
-  <div>
-    Insight Card
-  </div>
+
+const InsightCard = ({ imgUrl, title, subtitle, index }) => (
+  <motion.div
+    variants={fadeIn('up', 'spring', index * 0.5, 1)}
+    className="flex md:flex-row flex-col gap-4 w-[100%] h-[100%]"
+  >
+    <img
+      src={imgUrl}
+      alt="planet-01"
+      className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
+    />
+    <div className="w-full flex justify-between items-center">
+      <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px] gap-2">
+      </div>
+    </div>
+  </motion.div>
 );
 
-export default InsightCard, TitleText, subtitle
+export default InsightCard;
