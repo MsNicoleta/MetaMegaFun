72c26f6f111f11dc0fb741b0cb067b1f62966ea5
Nicoleta Grubi <msnicoleta@outlook.com>
2023-10-06 17:11:53 +0100

    insight section - added the text and the container limits

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -5,7 +5,7 @@ import { fadeIn } from '../utils/motion';
 const InsightCard = ({ imgUrl, title, subtitle, index }) => (
   <motion.div
     variants={fadeIn('up', 'spring', index * 0.5, 1)}
-    className="flex md:flex-row flex-col gap-4 w-[100%] h-[100%]"
+    className="flex flex flex flex-row gap-4 w-[100%] h-[100%]"
   >
     <img
       src={imgUrl}
@@ -13,7 +13,12 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
       className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
     />
     <div className="w-full flex justify-between items-center">
-      <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px] gap-2">
+      <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px]">
+        <h4 className=" font-normal lg:text-[42px] text-[26px] text-white">
+          {title}
+        </h4>
+        <p className=" mt-[16px] font-normal lg:text-[20px] text[14px] text-secondary-white">
+          {subtitle}</p>
       </div>
     </div>
   </motion.div>
