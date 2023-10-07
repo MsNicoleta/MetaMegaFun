ad75c616d9727390df4b84f9171aa403dc80964a
Nicoleta Grubi <msnicoleta@outlook.com>
2023-10-07 23:36:42 +0100

    insight section - fixing the alignment of the images

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -5,7 +5,7 @@ import { fadeIn } from '../utils/motion';
 const InsightCard = ({ imgUrl, title, subtitle, index }) => (
   <motion.div
     variants={fadeIn('up', 'spring', index * 0.5, 1)}
-    className="flex flex flex flex-row gap-4 w-[100%] h-[100%]"
+    className="flex md:flex-row flex-col gap-4 "
   >
     <img
       src={imgUrl}
@@ -13,7 +13,7 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
       className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
     />
     <div className="w-full flex justify-between items-center">
-      <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px]">
+      <div className="flex-1 md:ml-[62px] flex flex-col max-w-[500px]">
         <h4 className=" font-normal lg:text-[42px] text-[26px] text-white">
           {title}
         </h4>
