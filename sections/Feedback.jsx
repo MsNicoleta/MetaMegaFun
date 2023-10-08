5bcdd2c0353e39d193cf328d79e3fcfb1c09d94b
Nicoleta Grubi <msnicoleta@outlook.com>
2023-10-08 15:58:46 +0100

    feedback section - adding the containers and the arangement

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,8 +1,26 @@
 'use client';
 
+import { motion } from "framer-motion";
+import styles from '../styles'; // Import styles (ensure the correct path)
+import { fadeIn, staggerContainer, zoomIn } from '../utils/motion.js'; // Import motion variants
+
+
 const Feedback = () => (
   <section>
-    Feedback section
+    <motion.div
+      variants={staggerContainer} // Animation variants for staggering
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} flex mx-auto lg:flex-row flex-col gap-6`}
+    >
+      <motion.div
+        variants={fadeIn('right', 'tween', 0.2, 1)}
+        className="flex-[0.5] lg:max[370px] flex justify-end  flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative"
+      >
+
+      </motion.div>
+    </motion.div>
   </section>
 );
 
