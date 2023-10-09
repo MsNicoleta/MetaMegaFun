5ab938f187110ed654e0c98e42bb6b6a815eab5a
Nicoleta Grubi <msnicoleta@outlook.com>
2023-10-09 15:57:40 +0100

    feedback section - added the founder information and the hero image of the sectin and the stamp

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -16,17 +16,38 @@ const Feedback = () => (
     >
       <motion.div
         variants={fadeIn('right', 'tween', 0.2, 1)}
-        className="flex-[0.5] lg:max[370px] flex justify-end  flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative"
+        className="flex-[0.5] lg:max-w-[370px] flex justify-end  flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative ml-[3rem] mr-[3rem]"
       >
-        <div className="feedback-gradient">
-          <div>
-            <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[36px] text-white">Samantha</h4>
-            <p> Founder of MetaMegaFun</p>
-          </div>
+        <div className="feedback-gradient" />
+        <div>
+          <div className="font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[36px] text-white">Samantha</div>
+          <div className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22px] leading-[12px] text-white"> | Founder of MetaMegaFun</div>
         </div>
+        <div className="mt-[24px] font-normal sm:text-[24px] text-[12px] sm:leading-[45px] leading-[39px] text-white" >“With the development of today's technology, metaverse is very useful for today's work, or can be called web 3.0. by using metaverse you can use it as anything”</div>
+      </motion.div>
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className="flex-1  flex justify-center  rounded-[32px] relative ml-[0.5rem] mr-[3rem]"
+      >
+        <img
+          src='/planet-09.png'
+          alt='planet-09'
+          className='w-full rounded-[32px] lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]'
+        />
+        <motion.div
+          variations={zoomIn(0.4, 1)}
+          className="lg:block hidden absolute -left-[10%] top-[3%]"
+        >
+          <img
+            src="/stamp.png"
+            alt="stamp"
+            className="w-[155px] h-[155px] object-contain " />
+
+        </motion.div>
+
       </motion.div>
     </motion.div>
-  </section>
+  </section >
 );
 
 export default Feedback;
