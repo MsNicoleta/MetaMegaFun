3b7e37bd408b052ca4063f06be4b591edebf9d23
Nicoleta Grubi <msnicoleta@outlook.com>
2023-10-09 16:34:33 +0100

    footer section - starting

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,10 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { socials } from '../constants';
+import styles from '../styles';
+import { footerVariants } from '../utils/motion'; // Import animation variants from a utility file
+
 const Footer = () => (
   <footer>
     footer
